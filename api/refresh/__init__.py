5112314165395f5cca17f48990b0a1c1e271a3b9
Andrew Blumhardt <andrew@blumhardt.net>
2026-03-03 23:17:50 -0500

    going for broke

--- api/refresh/__init__.py
+++
@@ -1,8 +0,0 @@
-import azure.functions as func
-
-def main(req: func.HttpRequest) -> func.HttpResponse:
-    return func.HttpResponse(
-        body='{"status": "refresh endpoint reached"}',
-        mimetype="application/json",
-        headers={"Access-Control-Allow-Origin": "*"}
-    )
